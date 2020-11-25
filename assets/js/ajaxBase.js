74aa42b6b5c6b9face91d8354a492f58642dc14e
Fafa <Fafa@qq.com>
2020-11-25 10:17:42 +0800

    index页面完成

--- assets/js/ajaxBase.js
+++ assets/js/ajaxBase.js
@@ -1,6 +1,23 @@
 //filter拼成了fliter   = =...........
 $.ajaxPrefilter(function (options) {
-    options.url = "http://ajax.frontend.itheima.net" + options.url
+    options.url = "http://ajax.frontend.itheima.net" + options.url;
+
+    //处理请求头
+    if (options.url.indexOf("/my") !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token")
+        };
+        //登录校验
+        options.complete = function (xhr) {
+            let res = JSON.parse(xhr.responseText)
+            if (res.status === 1 && res.message === "身份认证失败！") {
+                console.log('fdsa');
+                localStorage.removeItem("token")
+                location.href = "login.html"
+            }
+        }
+
+    }
 });
 
 
---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+$(function () {
+
+    //获取用户信息
+    $.ajax({
+        url: "/my/userinfo",
+        success: function (res) {
+            let data = res.data;
+
+            if (res.status === 0) {
+                let $img = $('.layui-nav-img')
+                let $span = $('.text-avatar')
+                let name = (data.nickname || data.username);
+                $('#welcome-text').text("欢迎 " + name);
+                let W = name[0].toUpperCase();
+                if (data.user_pic) {
+                    $img.show().attr(src, data.user_pic);
+                    $span.hide()
+                } else {
+                    $img.hide()
+                    $span.text(W).show()
+                }
+            }
+        },
+        //身份认证失败跳转登录页面 登录校验，移至base中
+        //complete形参为JQ封装的xhr，responsText是字符串!
+        // complete: function (xhr) {
+        //     let res = JSON.parse(xhr.responseText)
+        //     if (res.status === 1 && res.message === "身份认证失败！") {
+        //         console.log('fdsa');
+        //         localStorage.removeItem("token")
+        //         location.href = "login.html"
+        //     }
+        // }
+    })
+
+})
